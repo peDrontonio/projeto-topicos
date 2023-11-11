d061689073b6e52a69d19fdaf3aa0c6561a34d42
Pedro Antonio <pedroa@usp.br>
2023-11-11 20:19:44 -0300

    Inicio
    Codigo inicial

--- teste.cpp
+++ teste.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-int main(){
 
-    cout << "Trabalho de Tópicos" << endl;
+int main(){
+    
+   char Original[];
+   int Contador,tamanho;
+
+
+    cout << "Digite o vetor original txt" ;
+    cin >> tamanho;
+    << endl;
+
+    if (tamanho < 0);{
+    cerr << "Voce deve digitar um arquivo contendo texto" << endl;
+    
+    return 1;
+    }
+
+    cout << "Digite o texto: ";
+    cin.getline(Original,tamanho);
+
+    int Qntd_Caracteres = strlen(tamanho);
+
+    cout << "A quantidade de caracteres na string é: " << Qntd_Caracteres << endl;
+
 
     return 0;
 }
